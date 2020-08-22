2b92985462d55083d925e007d44465cf6503fb5e
Ales1981 <vanerkaales@seznam.cz>
2020-08-22 17:44:24 +0200

    added .gitignore and RainbowBox

--- week-04/day-1/DrawingSession/src/RainbowBoxFunction.java
+++ week-04/day-1/DrawingSession/src/RainbowBoxFunction.java
@@ -11,11 +11,28 @@ public class RainbowBoxFunction {
         // and draws a square of that size and color to the center of the canvas.
         // Create a loop that fills the canvas with rainbow colored squares (red, orange, yellow, green, blue, indigo, violet).
 
-        String[] colors = {"red", "orange", "yellow", "green", "blue", "indigo", "violet"};
-        int firstSize = WIDTH;
+        Color colorIndigo = new Color(75, 0, 130);
+        Color colorViolet = new Color(238, 130, 238);
 
+        Color[] colors = { Color.red , Color.orange, Color.yellow, Color.green, Color.blue, colorIndigo, colorViolet};
+        int size = 300;
+
+        squares( size , colors , graphics);
 
     }
+
+    private static void squares(int size, Color[] colors, Graphics graphics) {
+        int count = 0;
+        for ( int i = size ; i > 0 ; i=i-(size/ colors.length)){
+            if( count < colors.length){
+                graphics.setColor(colors[count]);
+            }
+            graphics.fillRect((WIDTH-i)/2, (WIDTH-i)/2, i, i);
+            count++;
+        }
+
+    }
+
 
     // Don't touch the code below
     static int WIDTH = 320;
