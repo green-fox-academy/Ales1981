e8755694a27dff2c4e07d72f954ee485c6de8811
Ales1981 <vanerkaales@seznam.cz>
2020-09-01 19:13:41 +0200

    GFOrg Workshop

---
+++ week-07/day-1/inheritance/src/greenfoxinheritanceexcercise/Person.java
@@ -0,0 +1,27 @@
+package greenfoxinheritanceexcercise;
+
+public class Person {
+    protected String name;
+    protected int age;
+    protected String gender;
+
+    public Person(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Person(){
+        this.name = "Jane Doe";
+        this.age = 30;
+        this.gender = "female";
+    }
+
+    public void introduce(){
+        System.out.println("Hi, I'm "+name+", a "+age+" year(s) old "+gender+".");
+    }
+
+    public void getGoal(){
+        System.out.println("My goal is: Live for the moment!");
+    }
+}
