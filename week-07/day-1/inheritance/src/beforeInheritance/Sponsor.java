a8fa0a7d77b01607e489ce2343735bc53a50e90e
Ales1981 <vanerkaales@seznam.cz>
2020-09-01 19:08:05 +0200

    GFOrg excercise

---
+++ week-07/day-1/inheritance/src/beforeInheritance/Sponsor.java
@@ -0,0 +1,34 @@
+package beforeInheritance;
+
+public class Sponsor {
+    private String name;
+    private int age;
+    private String gender;
+    private String company;
+    private int hiredStudents;
+
+    public void introduce(){
+        System.out.println("Hi, I'm "+name+", a "+age+" year(s) old "+gender+", who represents "+company+
+                " and hired "+hiredStudents+" students so far.");
+    }
+    public void getGoal(){
+        System.out.println("Hire brilliant junior software developers.");
+    }
+    public void hire(){
+        hiredStudents++;
+    }
+    public Sponsor(String name, int age, String gender,String company){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.company = company;
+        hiredStudents = 0;
+    }
+    public Sponsor(){
+        this.name = "Jane Doe";
+        this.age = 30;
+        this.gender = "female";
+        this.company = "Google";
+        this.hiredStudents = 0;
+    }
+}
