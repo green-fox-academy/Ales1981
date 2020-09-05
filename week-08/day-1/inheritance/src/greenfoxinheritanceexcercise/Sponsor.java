f0262ad7447116acff0beaa04acbb6b9cad00aff
Ales1981 <vanerkaales@seznam.cz>
2020-09-05 15:26:30 +0200

    new order of folders

---
+++ week-08/day-1/inheritance/src/greenfoxinheritanceexcercise/Sponsor.java
@@ -0,0 +1,30 @@
+package greenfoxinheritanceexcercise;
+
+public class Sponsor extends Person {
+    private String company;
+    private int hiredStudents;
+
+    public Sponsor(String name, int age, String gender, String company){
+        super(name,age,gender);
+        this.company = company;
+        this.hiredStudents = 0;
+    }
+
+    public Sponsor(){
+        this.company = "Google";
+        this.hiredStudents = 0;
+    }
+
+    public void getGoal(){
+        System.out.println("Hire brilliant junior software developers.");
+    }
+
+    public void introduce(){
+        System.out.println("Hi, I'm "+name+", a "+age+" year(s) old "+gender+", who represents "+company+
+                " and hired "+hiredStudents+" students so far.");
+    }
+
+    public void hire(){
+        hiredStudents++;
+    }
+}
