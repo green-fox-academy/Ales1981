f22e2cf98c7a676a76488a5b295597a741a4ed90
Ales1981 <vanerkaales@seznam.cz>
2020-10-07 22:14:10 +0200

    added exercises 1-4 and gitignore file

---
+++ week-13/day-1/Stream/src/Exercise3.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        //Write a Stream Expression to find which number squared value is more then 20 from the following list:
+        List<Integer> numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
+        numbers.stream().forEach(number -> {
+            if (number * number > 20) System.out.println(number);
+        });
+    }
+}
